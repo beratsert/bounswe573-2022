7c7981b5a7996610d1c1358fc3ecd97cd9dda4cf
Berat Sert <berat.sert@getir.com>
2022-03-14 22:33:04 +0300

    retrieve data from DB

--- wikidata_web_app/get_data/views.py
+++ wikidata_web_app/get_data/views.py
@@ -1,17 +1,26 @@
 from ast import keyword
+from zoneinfo import available_timezones
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Entries
 import pywikibot
 
 # Create your views here.
+class Configuration:
+    source = 'wikipedia'
+    source_language = 'en'
+    available_languages = ['en', 'es', 'de', 'tr', 'fr']
+
 
 def home(request):
-    wpsite = pywikibot.Site('en', 'wikipedia')
-    wppage = pywikibot.Page(wpsite, 'basketball')
+    keyword = 'monkey'
+    wpsite = pywikibot.Site(Configuration.source_language, Configuration.source)
+    wppage = pywikibot.Page(wpsite, keyword)
     item = pywikibot.ItemPage.fromPage(wppage) 
     dictionary = item.get()
-    eng_description = dictionary['descriptions']['en']
-    entry1 = Entries.objects.create(keyword = 'basketball', description = eng_description, language = 'en')
-    entry1.save()
-    return HttpResponse(entry1.description)
+    
+    for lang in Configuration.available_languages:
+        entry = Entries.objects.create(keyword = keyword, description = dictionary['descriptions'][lang], language = lang)
+        entry.save()
+    Entries.objects.all()
+    return HttpResponse(Entries.objects.all()[1].description)
