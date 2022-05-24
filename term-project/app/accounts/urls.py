939c1bc8ef7e7a7ffe953472126a2ae8a2980936
Berat Sert <berat.sert@getir.com>
2022-05-24 17:29:09 +0300

    edit user credentials

--- term-project/app/accounts/urls.py
+++ term-project/app/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import EditView, SignUpView
 
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path("signup/", SignUpView.as_view(), name = "signup"),
+    path("edit_profile/", EditView.as_view(), name = "edit_profile")
 ]
--- term-project/app/accounts/views.py
+++ term-project/app/accounts/views.py
@@ -1,9 +1,18 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import SignUpForm
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+class EditView(generic.UpdateView):
+    form_class = UserChangeForm
+    success_url = reverse_lazy("home")
+    template_name = "registration/edit_profile.html"
+
+    def get_object(self):
+        return self.request.user
