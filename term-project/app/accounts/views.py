d680b41fffe28ae13f08a52623d5defb243e6b62
Berat Sert <berat.sert@getir.com>
2022-05-24 17:54:45 +0300

    edit profile page added

--- term-project/app/accounts/views.py
+++ term-project/app/accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 
 class SignUpView(generic.CreateView):
@@ -10,7 +10,7 @@ class SignUpView(generic.CreateView):
     template_name = "registration/signup.html"
 
 class EditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     success_url = reverse_lazy("home")
     template_name = "registration/edit_profile.html"
 
